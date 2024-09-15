ff6551ae21c0c3b2c1ea7db689dd75df94fb8b9c
桜羽 明人 <114555177+AkitoSakurabaCreator@users.noreply.github.com>
2024-09-15 23:39:20 +0900

    upload
    upload

--- chat-app/src/App.tsx
+++ chat-app/src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./App.scss";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
-  return <div className="App">
-    <h1>Hello</h1>
+  return<div className="App">
+    {/* Sidebar */}
+    <Sidebar></Sidebar>
+    {/* Home */}
   </div>;
 }
 
---
+++ chat-app/src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import "./Sidebar.scss";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from './SidebarChannel';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* sidebarLeft */}
+      <div className="sidebarLeft">
+          <div className="serverIcon">
+            <img src="./logo192.png" alt="" />
+          </div>
+          <div className="serverIcon">
+            <img src="./logo192.png" alt="" />
+          </div>
+      </div>
+      {/* sidebarRight */}
+      <div className="sidebarRight">
+        <div className="sidebarTop">
+          <h3>Hello</h3>
+          <ExpandMoreIcon></ExpandMoreIcon>
+        </div>
+        {/* sidebarChannels */}
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+            <ExpandMoreIcon></ExpandMoreIcon>
+            <h4>テストチャンネル</h4>
+            </div>
+            <AddIcon className="sidebarAddIcon"></AddIcon>
+          </div>
+
+          <div className="sidebarChannelList">
+            <SidebarChannel></SidebarChannel>
+            <SidebarChannel></SidebarChannel>
+            <SidebarChannel></SidebarChannel>
+            <SidebarChannel></SidebarChannel>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
