90cb7fe0f121a88dbf8259098e14370e74469701
桜羽 明人 <sakurabaakito@kaede-creation.com>
2024-10-26 19:39:27 +0900

    チャットメッセージのレイアウトを作成

---
+++ chat-app/src/components/chat/ChatMessage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ChatMessage.scss"
+import {Avatar} from "@mui/material";
+
+const ChatMessage = () => {
+    return <div className="message">
+        <Avatar></Avatar>
+        <div className="messageInfo">
+            <h4>Tester
+                <span className="messageTimestamp">2024/10/26</span>
+            </h4>
+
+            <p>メッセージ本文</p>
+            
+        </div>
+    </div>;
+};
+
+export default ChatMessage;
