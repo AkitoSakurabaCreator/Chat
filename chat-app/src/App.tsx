e84c42b23e5608cfdd6385e3fe1e308e2a33251f
桜羽 明人 <sakurabaakito@kaede-creation.com>
2024-10-26 18:28:23 +0900

    チャットメッセージを送るコンポーネントを追加
    チャットメッセージを送るコンポーネントを追加

--- chat-app/src/App.tsx
+++ chat-app/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.scss";
+import Chat from "./components/chat/Chat";
 import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return<div className="App">
     {/* Sidebar */}
     <Sidebar></Sidebar>
-    {/* Home */}
+    {/* Chat */}
+    <Chat></Chat>
   </div>;
 }
 
