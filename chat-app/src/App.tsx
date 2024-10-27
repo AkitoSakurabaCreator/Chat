f2558e33918cceba87c39f822444eb5c7e7e29a2
桜羽 明人 <sakurabaakito@kaede-creation.com>
2024-10-27 23:58:26 +0900

    ログインとログアウト機能を追加

--- chat-app/src/App.tsx
+++ chat-app/src/App.tsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
 import Chat from "./components/chat/Chat";
 import Sidebar from "./components/sidebar/Sidebar";
+import Login from "./components/login/Login";
+import { auth } from "./firebase";
+import { login, logout } from "./features/userSlice";
 
 function App() {
-  return<div className="App">
-    {/* Sidebar */}
-    <Sidebar></Sidebar>
-    {/* Chat */}
-    <Chat></Chat>
-  </div>;
+  const user = useAppSelector((state) => state.user);
+  // const user = null;
+  // console.log(user);
+
+  const dispatch = useAppDispatch();
+
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      console.log(loginUser);
+      if (loginUser) {
+        dispatch(
+          login({
+            uid: loginUser.uid,
+            displayName: loginUser.displayName,
+            email: loginUser.email,
+            photo: loginUser.photoURL,
+          })
+        );
+      }else{
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
+  return(
+  <div className="App">
+    { user ?(
+      <>
+        <Sidebar></Sidebar>
+        <Chat></Chat>
+      </>
+    ) : (
+      <>
+        <Login></Login>
+      </>
+    )}
+  </div>
+  );
 }
 
 export default App;
