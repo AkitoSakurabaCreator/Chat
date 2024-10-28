8a3cf01a866ca3b84891f39a78f997dceb96fecb
桜羽 明人 <sakurabaakito@kaede-creation.com>
2024-10-28 23:07:28 +0900

    channelReducerを追記

--- chat-app/src/app/store.ts
+++ chat-app/src/app/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
+import { channel } from "diagnostics_channel";
+import channelReducer from "../features/channelSlice";
 
 export const store = configureStore({
-    reducer: userReducer,
+    reducer: {
+        user: userReducer,
+        channel: channelReducer,
+    },
 });
 
 export type AppDispatch = typeof store.dispatch;
-
 export type RootState = ReturnType<typeof store.getState>;
