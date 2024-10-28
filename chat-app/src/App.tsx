834e7af25bb319e81200e92fc6c2b2ae4b12b055
桜羽 明人 <sakurabaakito@kaede-creation.com>
2024-10-28 23:34:30 +0900

    Reducerを変更したときの反映user=>user.user

--- chat-app/src/App.tsx
+++ chat-app/src/App.tsx
@@ -37,7 +37,7 @@ function App() {
 
   return(
   <div className="App">
-    { user ?(
+    { user.user ?(
       <>
         <ErrorBoundary FallbackComponent={ErrorFallBack}>
           <Sidebar></Sidebar>
