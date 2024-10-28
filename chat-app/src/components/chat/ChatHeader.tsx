b0ea2ad1b121bc836bcb5357d2c7a391b2339728
桜羽 明人 <sakurabaakito@kaede-creation.com>
2024-10-28 23:06:54 +0900

    チャンネル名をクリックされたらfirebaseにあるチャンネル名を表示

--- chat-app/src/components/chat/ChatHeader.tsx
+++ chat-app/src/components/chat/ChatHeader.tsx
@@ -7,12 +7,19 @@ import SearchIcon from '@mui/icons-material/Search';
 import SendIcon from '@mui/icons-material/Send';
 import HelpIcon from '@mui/icons-material/Help';
 
-const ChatHeader = () => {
-    return <div className="chatHeader">
+type Props = {
+    channelName: string | null;
+};
+
+const ChatHeader = (props: Props) => {
+    const { channelName } = props;
+
+    return (
+    <div className="chatHeader">
         <div className="chatHeaderLeft">
             <h3>
                 <span className="chatHeaderHash">#</span>
-                TEST
+                {channelName}
             </h3>
         </div>
 
@@ -28,7 +35,8 @@ const ChatHeader = () => {
             <SendIcon></SendIcon>
             <HelpIcon></HelpIcon>
         </div>
-    </div>;
+    </div>
+    );
 };
 
 export default ChatHeader;
